12ed656b202bd1c15ba94a0c87ddd603dfe9f847
Friedrich42 <murik_bro@mail.ru>
2020-01-15 20:05:34 +0400

    beautifying

--- yamtoelite.py
+++ yamtoelite.py
@@ -10,28 +10,19 @@ from TrackDB import Track, TrackDB
 from functions import *
 
 bot = telebot.TeleBot(conf.API_TOKEN_TELEGRAM)
-client = Client(conf.API_TOKEN_YANDEX_MUSIC_CLIENT)
+yam_client = Client(conf.API_TOKEN_YANDEX_MUSIC_CLIENT)
 track_db = TrackDB()
 
 main_channel_id = conf.CHANNEL_ID_FOR_BOT
 
 
-def get_songs_from_yamusic():
-    # liked = client.users_likes_tracks()
-    # tracks = []
-    # for song in liked.tracks:
-    #     tracks.append(Track(id_of_song=song.track.id,
-    #                         song_name=song.track.title,
-    #                         artists=" ft. ".join([artist.name for artist in song.track.artists]),
-    #                         album_id=song.album_id,
-    #                         local_path="",))
-    # return tracks
+def get_songs_from_yamusic(yandex_music_client):
     try:
         return list([Track(id_of_song=song.track.id,
                            song_name=song.track.title,
                            artists=" ft. ".join([artist.name for artist in song.track.artists]),
                            album_id=song.album_id,
-                           local_path="", ) for song in client.users_likes_tracks().tracks])
+                           local_path="", ) for song in yandex_music_client.users_likes_tracks().tracks])
     except Exception as e:
         log_error(e)
 
@@ -48,13 +39,13 @@ def on_new_song_detected(song):
 def worker():
     try:
         while True:
-            for song in get_songs_from_yamusic():
+            for song in get_songs_from_yamusic(yam_client):
                 db_song = track_db.get_song(song.id_of_song)
                 if db_song is None:
                     track_name = f"{song.artists} - {song.song_name}.mp3"
                     with in_dir(f"tracks/{song.artists}"):
                         sleep(5)
-                        client.tracks([f'{song.id_of_song}:{song.album_id}'])[0].download(track_name)
+                        yam_client.tracks([f'{song.id_of_song}:{song.album_id}'])[0].download(track_name)
                     song.local_path = f"tracks/{song.artists}/{track_name}"
                     track_db.add_song(song)
 
