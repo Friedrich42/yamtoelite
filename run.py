dd44af2867a82f53cb360260ba6856fe0b7abeda
Friedrich Albert Kyuri <friedrichak42@gmail.com>
2020-03-15 15:41:45 +0400

    fixed logging directory

--- run.py
+++ run.py
@@ -2,13 +2,17 @@ import logging
 
 from src import conf
 from src import config
+from src import functions as f
 from src.main_ym2tch import main
 
 
 def launch_app():
     db_session = config(conf.DB_PATH)
     logger = logging.getLogger('main')
-    logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)-15s %(name)s - %(levelname)s - %(message)s')
+    with f.in_dir("logs"):
+        logging.basicConfig(filename=f'app-{f.get_time()}.log', filemode='a', format='%(asctime)-15s %(name)s'
+                                                                                     ' - %(levelname)s - %(message)s')
+    logger.setLevel("DEBUG")
 
     main(db_session, logger)
 
